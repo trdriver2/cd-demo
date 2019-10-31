bc854c359f4407233cc0d6331ea54f8807f33492
tyler <tdriver@talentpath.com>
2019-10-31 14:26:17 -0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>THis is a test</p>
+    </div>
+  );
 }
 
 export default App;
